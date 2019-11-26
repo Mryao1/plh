334ef93c92cb05ba6f92f520061e6dd64d8bd9e6
HsqSw <502464113@qq.com>
2019-11-26 11:31:36 +0800

    数据库

--- project/myapp/admin.py
+++ project/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserInfo,Plant,PlantDisease
 # Register your models here.
+admin.site.register(PlantDisease)
+admin.site.register(UserInfo)
+admin.site.register(Plant)
--- project/project/settings.py
+++ project/project/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/assets/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
